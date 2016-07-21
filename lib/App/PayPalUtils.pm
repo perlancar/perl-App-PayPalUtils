53479cbd78f2ce0c5a2156342b45ed91812a18cd
perlancar (@pc-office) <perlancar@gmail.com>
2016-07-21 19:58:07 +0700

    Initial commit

---
+++ lib/App/PayPalUtils.pm
@@ -0,0 +1,20 @@
+package App::PayPalUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+#ABSTRACT: CLI utilities related to PayPal
+
+=head1 DESCRIPTION
+
+This distribution includes a few CLI utilities related to PayPal:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
